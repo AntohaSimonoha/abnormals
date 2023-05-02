26749dd2f7ff049c65dd493d3ff68f7530e1a6a3
AntohaSimonoha <ilja.serenko@gmail.com>
2023-05-02 17:56:04 +0300

    Functionality added to compilevid.sh

--- scripts/compilevid.sh
+++ scripts/compilevid.sh
@@ -1,3 +1,19 @@
 #! /bin/bash
 
-#ffmpeg -start_number 1 -framerate 30 -i renamed/%03d.png -vcodec prores_ks -pix_fmt yuva444p10 -vendor ap10 -profile:v 4 video.mov
+PNG_DIR='../png/'
+
+for ENTRY in "$PNG_DIR"*/*/
+do
+	NAME=$(echo "$ENTRY" | cut -d"/" -f3);
+	echo "$NAME";
+
+	PATH_TO_FILE="../png/$NAME/Artistinimi/%04d.png"
+	echo "$PATH_TO_FILE"
+
+	PATH_TO_SAVE="../videos/$NAME.mov"
+	echo "$PATH_TO_SAVE"
+
+	ffmpeg -start_number 1 -framerate 30 -i "$PATH_TO_FILE"\
+	-vcodec prores_ks -pix_fmt yuva444p10 -vendor ap10\
+       	-profile:v 4 "$PATH_TO_SAVE"
+done
