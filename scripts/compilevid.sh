f7f20a15bfcf2063fdf7784e91cb0c30f37d1451
AntohaSimonoha <ilja.serenko@gmail.com>
2023-05-19 19:22:52 +0300

    'encode' function takes in parameters now. Starting and stoping frame parameters added

--- scripts/compilevid.sh
+++ scripts/compilevid.sh
@@ -9,10 +9,16 @@ CODEC="prores_ks"
 PIXFMT="yuva444p10"
 VENDOR="ap10"
 PROFILE_NUM=4
+START=1
+END=400
 
 function encode {
 
-	ffmpeg -start_number 1\
+	START_NUMBER=$1
+	END_NUMBER=$2
+	PATH_TO_SAVE=$3
+
+	ffmpeg -start_number "$START_NUMBER"\
 	-framerate "$FPS"\
        	-i "$PATH_TO_FILE"\
 	-vcodec "$CODEC"\
@@ -20,6 +26,7 @@ function encode {
        	-vendor "$VENDOR"\
 	-vf scale="$RES"\
        	-profile:v "$PROFILE_NUM"\
+	-frames:v "$END_NUMBER"\
        	"$PATH_TO_SAVE"
 }
 
@@ -31,9 +38,17 @@ do
 	PATH_TO_FILE="../png/$NAME/%04d.png"
 	echo "$PATH_TO_FILE"
 
-	PATH_TO_SAVE="../videos/$NAME.mov"
+	PATH_TO_SAVE_FULL="../videos/"$NAME"FULL.mov"
 	echo "$PATH_TO_SAVE"
 
-	encode
+	PATH_TO_SAVE_START="../videos/"$NAME"START.mov"
+	echo "$PATH_TO_SAVE"
+
+	PATH_TO_SAVE_END="../videos/"$NAME"END.mov"
+	echo "$PATH_TO_SAVE"
+
+	encode "$START" "$END" "$PATH_TO_SAVE_FULL"
+	encode "$(($START + 200))" "$END" "$PATH_TO_SAVE_START"
+	encode "$START" "$(($END - 200))" "$PATH_TO_SAVE_END"
 done
 
